af1c0826e82b14b22976cd24a142e404d5b3a204
clay3075 <cjreddick96@gmail.com>
2016-03-30 12:36:27 -0500

    Changed server to serve static html with css

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 var express = require('express');
 var app = express(); //create express object
 
-app.get('/', function (req, res) {  //create get route for root directory
+/*app.get('/', function (req, res) {  //create get route for root directory
   res.send('Hello World!');
-});
+});*/
+
+app.use('/', express.static(__dirname + '/public'));
 
 app.listen(3000, function () { //start listening for activity on port 3000
   console.log('Example app listening on port 3000!');
