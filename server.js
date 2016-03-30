e5c73b22eecf4fe999af0e6a56b72e59898ae1d4
clay3075 <cjreddick96@gmail.com>
2016-03-30 12:13:41 -0500

    Hello world server using express and node

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+//hello world server using express beginning of project
+var express = require('express');
+var app = express(); //create express object
+
+app.get('/', function (req, res) {  //create get route for root directory
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () { //start listening for activity on port 3000
+  console.log('Example app listening on port 3000!');
+});
