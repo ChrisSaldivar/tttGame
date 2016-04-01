fca51f2804ac8e02486cf403630b1c17f79fcfbc
clay3075 <cjreddick96@gmail.com>
2016-04-01 13:12:51 -0500

    Just in case

--- public/js/game.js
+++ public/js/game.js
@@ -1,66 +1,77 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'TTT', { preload: preload, create: create});
+var ws = new WebSocket('ws://localhost:3000/game');
+ws.onopen = function() {
+    ws.send("Connection good.");
+};
 
-
-function preload(){
-	game.load.spritesheet('button', 'assets/button_spritesheet.png', 105, 90, 3);
-	game.load.image('background','assets/board.png');
+ws.onclose = function() {
+    ws.send("Connection is closed...");
 }
 
-var background;
-var currentPlayer = "X";
-var moveNumber = 1;
+function run() {
+    var game = new Phaser.Game(700, 500, Phaser.AUTO, 'TTT', { preload: preload, create: create});
 
-function create (){
 
-    background = game.add.tileSprite(0, 0, 800, 600, 'background');
-    
-	generateButtons();
 
-}
-
-function generateButtons(){
-    var button;
-    var x = 150;
-    var y = 75;
-    for (var i = 0; i < 3; i++){
-        for (var j=0; j<3; j++){
-            button = game.add.button(x, y, 'button', actionOnClick, this);
-            button.name = "Button " + (j + 3*i + 1);
-            button.col = i;
-            button.row = j;
-            x += 150;
-        }
-        x = 150
-        y += 150;
+    function preload(){
+	   game.load.spritesheet('button', 'assets/button_spritesheet.png', 105, 90, 3);
+	   game.load.image('background','assets/board.png');
     }
-}
 
-function actionOnClick(button){
-	changeFrame(button);
-	var winner = ttt.checkWin();
-	if (winner === "" && moveNumber == 10){
-        game.add.text(game.world.centerX, game.world.centerY, "It's a tie", { font: "65px Arial", fill: "#ff0044", align: "center" });
-        setTimeout(reset, 3000);
-        moveNumber = 1;
-	}
-	if (winner !== ""){
-        game.add.text(game.world.centerX, game.world.centerY, "winner is: " + winner, { font: "65px Arial", fill: "#ff0044", align: "center" });
-        setTimeout(reset, 3000);
-        moveNumber = 1;
-	}
-	
-}
+    var background;
+    var currentPlayer = "X";
+    var moveNumber = 1;
 
-function changeFrame(button){
-    if (ttt.playMove(button.row, button.col, currentPlayer)){
-        var frame = (currentPlayer === "X") ? 1 : 2;
-        button.setFrames(frame);
-        currentPlayer = (currentPlayer === "X") ? "O" : "X";
-        moveNumber++;
-	}
-}
+    function create (){
+        gameOver = false;
 
-function reset(){
-    ttt.reset();
-    create();
+        background = game.add.tileSprite(0, 0, 800, 600, 'background');
+    
+	    generateButtons();
+
+    }
+    function generateButtons(){
+        var button;
+        var x = 150;
+        var y = 75;
+        for (var i = 0; i < 3; i++){
+            for (var j=0; j<3; j++){
+                button = game.add.button(x, y, 'button', actionOnClick, this);
+                button.name = "Button " + (j + 3*i + 1);
+                button.col = i;
+                button.row = j;
+                x += 150;
+            }
+            x = 150
+            y += 150;
+        }
+    }
+
+    function actionOnClick(button){
+	   changeFrame(button);
+	   var winner = ttt.checkWin();
+	   if (winner === "" && moveNumber == 10){
+            game.add.text(game.world.centerX, game.world.centerY, "It's a tie", { font: "65px Arial", fill: "#ff0044", align: "center" });
+            setTimeout(reset, 3000);
+            moveNumber = 1;
+	   }
+	   if (winner !== ""){
+            game.add.text(game.world.centerX, game.world.centerY, "winner is: " + winner, { font: "65px Arial", fill: "#ff0044", align: "center" });
+            setTimeout(reset, 3000);
+            moveNumber = 1;
+	   }
+    }
+
+    function changeFrame(button){
+        if (ttt.playMove(button.row, button.col, currentPlayer)){
+            var frame = (currentPlayer === "X") ? 1 : 2;
+            button.setFrames(frame);
+            currentPlayer = (currentPlayer === "X") ? "O" : "X";
+            moveNumber++;
+	   }
+    }
+
+    function reset(){
+        ttt.reset();
+        create();
+    }
 }
