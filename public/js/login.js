f5aef103b9f67d07f016f87e2c898663d577aac8
Chris Saldivar <christop.saldivar@smail.astate.edu>
2016-04-08 00:07:16 +0000

    Added more to login.js and added ws handling to server (untested)

--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,38 @@
+var ws = new WebSocket('ws://chrisds.koding.io:3000/auth');
+
 function submitInfo(){
-    console.log('hi');
-    window.location = 'http://uckk7ec065b1.chrisds.koding.io/main.html';
+    var message = {
+        cmd:      'login',
+        username: 'user',
+        password: 'pass'
+    };
+    ws.send(message);
+}
+
+ws.onopen = function() {
+    var msg = {status: "Connection good."};
+    ws.send(JSON.stringify(msg));
+};
+
+ws.onmessage = function(event) {
+    var msg = JSON.parse(event.data);
+    if (msg.redirect){
+        redirect(msg);
+    }
+    else{
+        displayError();
+    }
+};
+
+function elt(id){
+    return document.getElementById(id);
+}
+
+
+function redirect(msg){
+    window.location = msg.url;
+}
+
+function displayError(){
+    
 }
